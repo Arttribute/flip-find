b920baf9431696237a4d480439a9847d9b867504
MartinWachira <67173409+mmwachira@users.noreply.github.com>
2024-07-18 16:59:39 +0300

    feat: connected game to playfab

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
 
@@ -7,10 +8,15 @@ public class GameManager : MonoBehaviour
     public static GameManager instance;
     public GameObject cardPrefab;
     public Transform gridTransform;
+    public Transform fullImageTransform;
+    public Image fullImageDisplay;
+    public Text scoreText;
     public Sprite[] cardImages;
+    public Sprite[] fullImages;
     private List<Card> cards = new List<Card>();
     private Card firstFlippedCard;
     private Card secondFlippedCard;
+    private int score = 0;
 
     void Awake()
     {
@@ -31,20 +37,45 @@ public class GameManager : MonoBehaviour
 
     void GenerateCards()
     {
-        List<Sprite> images = new List<Sprite>();
-        images.AddRange(cardImages); // Duplicate images for pairs
-        images.AddRange(cardImages);
+        List<Sprite> splitImages = new List<Sprite>(cardImages);
+        List<Sprite> correspondingFullImages = new List<Sprite>(fullImages);
+        List<int> usedIndexes = new List<int>();
+
+        //List<Sprite> images = new List<Sprite>();
+        //images.AddRange(cardImages); // Duplicate images for pairs
+        //images.AddRange(cardImages);
 
         // Shuffle images
-        images = ShuffleList(images);
+        //images = ShuffleList(images);
 
-        for (int i = 0; i < images.Count; i++)
+        // for (int i = 0; i < images.Count; i++)
+        // {
+        //     GameObject cardObject = Instantiate(cardPrefab, gridTransform);
+        //     Card card = cardObject.GetComponent<Card>();
+        //     card.SetCardImage(images[i]);
+        //     cards.Add(card);
+        // }
+
+        foreach (Sprite image in cardImages)
         {
-            GameObject cardObject = Instantiate(cardPrefab, gridTransform);
-            Card card = cardObject.GetComponent<Card>();
-            card.SetCardImage(images[i]);
-            cards.Add(card);
+            int index = Random.Range(0, correspondingFullImages.Count);
+            while (usedIndexes.Contains(index))
+            {
+                index = Random.Range(0, correspondingFullImages.Count);
+            }
+            usedIndexes.Add(index);
+
+            Sprite fullImage = correspondingFullImages[index];
+            CreateCard(image, fullImage);
+            CreateCard(image, fullImage);
         }
+    }
+
+    void CreateCard(Sprite frontImage, Sprite fullImage)
+    {
+        GameObject cardObject = Instantiate(cardPrefab, gridTransform);
+        Card card = cardObject.GetComponent<Card>();
+        card.SetCardImages(frontImage, fullImage);
     }
 
     public void OnCardFlipped(Card flippedCard)
@@ -64,12 +95,14 @@ public class GameManager : MonoBehaviour
     {
         yield return new WaitForSeconds(1);
 
-        if (firstFlippedCard.cardFront == secondFlippedCard.cardFront)
+        if (firstFlippedCard.fullImage == secondFlippedCard.fullImage)
         {
-            firstFlippedCard.isMatched = true;
-            secondFlippedCard.isMatched = true;
-           /*UIManager.Instance.AddScore(10); // Add points for a correct match
-            CheckLevelCompletion();*/
+            // Match found, show the full image
+            fullImageDisplay.sprite = firstFlippedCard.fullImage;
+            score += 25;
+            scoreText.text = "Score: " + score;
+            /*UIManager.Instance.AddScore(10); // Add points for a correct match
+             CheckLevelCompletion();*/
         }
         else
         {
