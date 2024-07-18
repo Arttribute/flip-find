c58ade2b319c7c7a3ba2b2f30264ef8b6e24ba9a
MartinWachira <67173409+mmwachira@users.noreply.github.com>
2024-07-19 01:31:39 +0300

    feat: show image on match

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -1,56 +1,61 @@
-
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Card : MonoBehaviour
 {
-
-    public Sprite cardFront;
-    public Sprite cardBack;
-    public Sprite fullImage;
+    public Sprite cardFront; // Image of the card's front
+    public Sprite cardBack; // Image of the card's back
     private Image image;
     private bool isFlipped = false;
-    public bool isMatched = false;
+    [SerializeField] private bool isMatched = false;
 
-    void Start()
+    private void Start()
     {
         image = GetComponent<Image>();
         ShowCardBack();
-
-        // Add Button Click Listener
         GetComponent<Button>().onClick.AddListener(OnCardClicked);
-
     }
 
-    public void SetCardImages(Sprite frontImage, Sprite fullImage)
+    public void SetCardImage(Sprite frontImage)
     {
         cardFront = frontImage;
-        this.fullImage = fullImage;
     }
 
     public void ShowCardFront()
     {
-        image.sprite = cardFront;
-        isFlipped = true;
+        if (!isMatched)
+        {
+            image.sprite = cardFront;
+            isFlipped = true;
+        }
+
     }
 
     public void ShowCardBack()
     {
-        image.sprite = cardBack;
-        isFlipped = false;
+        if (!isMatched)
+        {
+            image.sprite = cardBack;
+            isFlipped = false;
+        }
+
     }
 
     public void OnCardClicked()
     {
-
         if (!isFlipped)
         {
             ShowCardFront();
-            GameManager.instance.OnCardFlipped(this);
-            Debug.Log("Card flipped");
-
+            GameManager.Instance.OnCardFlipped(this);
         }
     }
 
-
+    public void SetMatched()
+    {
+        isMatched = true;
+        Color newColor;
+        ColorUtility.TryParseHtmlString("#1A0033", out newColor);
+        image.sprite = null;
+        image.color = newColor;
+    }
 }
