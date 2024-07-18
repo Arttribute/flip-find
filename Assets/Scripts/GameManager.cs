c0ed338f948503c762602b10abdd990db66566b9
MartinWachira <67173409+mmwachira@users.noreply.github.com>
2024-07-19 01:31:39 +0300

    feat: show image on match

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,29 +5,18 @@ using System.Collections.Generic;
 
 public class GameManager : MonoBehaviour
 {
-    public static GameManager instance;
+    public static GameManager Instance;
     public GameObject cardPrefab;
     public Transform gridTransform;
-    public Transform fullImageTransform;
-    public Image fullImageDisplay;
-    public Text scoreText;
+    public Transform fullImageTransform; // Reference to the UI element to show full image
+    public Image fullImageDisplay; // Image component to display the full image
     public Sprite[] cardImages;
-    public Sprite[] fullImages;
-    private List<Card> cards = new List<Card>();
     private Card firstFlippedCard;
     private Card secondFlippedCard;
-    private int score = 0;
 
-    void Awake()
+    private void Awake()
     {
-        if (instance == null)
-        {
-            instance = this;
-        }
-        else
-        {
-            Destroy(gameObject);
-        }
+        Instance = this;
     }
 
     void Start()
@@ -37,81 +26,16 @@ public class GameManager : MonoBehaviour
 
     void GenerateCards()
     {
-        List<Sprite> splitImages = new List<Sprite>(cardImages);
-        List<Sprite> correspondingFullImages = new List<Sprite>(fullImages);
-        List<int> usedIndexes = new List<int>();
+        List<Sprite> images = new List<Sprite>(cardImages);
+        images.AddRange(cardImages); // Duplicate images for pairs
+        images = ShuffleList(images);
 
-        //List<Sprite> images = new List<Sprite>();
-        //images.AddRange(cardImages); // Duplicate images for pairs
-        //images.AddRange(cardImages);
-
-        // Shuffle images
-        //images = ShuffleList(images);
-
-        // for (int i = 0; i < images.Count; i++)
-        // {
-        //     GameObject cardObject = Instantiate(cardPrefab, gridTransform);
-        //     Card card = cardObject.GetComponent<Card>();
-        //     card.SetCardImage(images[i]);
-        //     cards.Add(card);
-        // }
-
-        foreach (Sprite image in cardImages)
+        foreach (Sprite image in images)
         {
-            int index = Random.Range(0, correspondingFullImages.Count);
-            while (usedIndexes.Contains(index))
-            {
-                index = Random.Range(0, correspondingFullImages.Count);
-            }
-            usedIndexes.Add(index);
-
-            Sprite fullImage = correspondingFullImages[index];
-            CreateCard(image, fullImage);
-            CreateCard(image, fullImage);
+            GameObject cardObject = Instantiate(cardPrefab, gridTransform);
+            Card card = cardObject.GetComponent<Card>();
+            card.SetCardImage(image);
         }
-    }
-
-    void CreateCard(Sprite frontImage, Sprite fullImage)
-    {
-        GameObject cardObject = Instantiate(cardPrefab, gridTransform);
-        Card card = cardObject.GetComponent<Card>();
-        card.SetCardImages(frontImage, fullImage);
-    }
-
-    public void OnCardFlipped(Card flippedCard)
-    {
-        if (firstFlippedCard == null)
-        {
-            firstFlippedCard = flippedCard;
-        }
-        else if (secondFlippedCard == null)
-        {
-            secondFlippedCard = flippedCard;
-            StartCoroutine(CheckForMatch());
-        }
-    }
-
-    IEnumerator CheckForMatch()
-    {
-        yield return new WaitForSeconds(1);
-
-        if (firstFlippedCard.fullImage == secondFlippedCard.fullImage)
-        {
-            // Match found, show the full image
-            fullImageDisplay.sprite = firstFlippedCard.fullImage;
-            score += 25;
-            scoreText.text = "Score: " + score;
-            /*UIManager.Instance.AddScore(10); // Add points for a correct match
-             CheckLevelCompletion();*/
-        }
-        else
-        {
-            firstFlippedCard.ShowCardBack();
-            secondFlippedCard.ShowCardBack();
-        }
-
-        firstFlippedCard = null;
-        secondFlippedCard = null;
     }
 
     List<Sprite> ShuffleList(List<Sprite> list)
@@ -125,22 +49,64 @@ public class GameManager : MonoBehaviour
         }
         return list;
     }
-    /*void CheckLevelCompletion()
+
+    public void OnCardFlipped(Card card)
     {
-        bool isMatched = true;
-
-        foreach (Card card in cards)
+        if (firstFlippedCard == null)
         {
-            if (!card.isMatched)
-            {
-                isMatched = false;
-                break;
-            }
+            firstFlippedCard = card;
+        }
+        else if (secondFlippedCard == null)
+        {
+            secondFlippedCard = card;
+            StartCoroutine(CheckForMatch());
+        }
+    }
+
+    private IEnumerator CheckForMatch()
+    {
+        yield return new WaitForSeconds(1); // Wait for a second to let the player see the flipped cards
+
+        if (firstFlippedCard.cardFront == secondFlippedCard.cardFront)
+        {
+
+            // Match found
+            // Here you can add code to disable or remove the matched cards
+            fullImageDisplay.sprite = firstFlippedCard.cardFront;
+
+            firstFlippedCard.SetMatched();
+            secondFlippedCard.SetMatched();
+
+            //firstFlippedCard.gameObject.SetActive(false);
+            //secondFlippedCard.gameObject.SetActive(false);
+        }
+        else
+        {
+            // No match, flip cards back
+            firstFlippedCard.ShowCardBack();
+            secondFlippedCard.ShowCardBack();
         }
 
-        if (isMatched)
-        {
-            CollectablesManager.instance.OnLevelUp();
-        }
-    }*/
+        firstFlippedCard = null;
+        secondFlippedCard = null;
+    }
 }
+
+//void CheckLevelCompletion()
+// {
+//     bool isMatched = true;
+
+//     foreach (Card card in cards)
+//     {
+//         if (!card.isMatched)
+//         {
+//             isMatched = false;
+//             break;
+//         }
+//     }
+
+//     if (isMatched)
+//     {
+//         CollectablesManager.instance.OnLevelUp();
+//     }
+// }
