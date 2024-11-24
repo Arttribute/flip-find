9e8d590840eda0bddd8fa0f2a2684db0590bd574
MartinWachira <67173409+mmwachira@users.noreply.github.com>
2024-11-24 21:46:56 +0200

    refactor: replaced some backgrounds and images

--- Assets/Scripts/MenuAnim.cs
+++ Assets/Scripts/MenuAnim.cs
@@ -8,7 +8,7 @@ public class MenuAnim : MonoBehaviour
     [SerializeField] private RectTransform menuTransform;
     [SerializeField] private float showAnimationDuration = 0.5f;
     [SerializeField] private float hideAnimationDuration = 0.2f; // Faster hide animation duration
-    [SerializeField] private Vector2 hiddenPosition = new Vector2(-500f, 0); // Example hidden position
+    [SerializeField] private Vector2 hiddenPosition = new Vector2(-1000f, 0); // Example hidden position
     [SerializeField] private Vector2 shownPosition = new Vector2(0, 0); // Example shown position
 
     private bool isMenuShown = false;
